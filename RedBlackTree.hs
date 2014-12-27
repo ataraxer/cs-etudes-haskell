8aed75959163404b07a3d743ba7e85be70e89305
Anton Karamanov <ataraxer@gmail.com>
2014-12-27 23:42:09 +0300

    RBT: simplify implementation

--- RedBlackTree.hs
+++ RedBlackTree.hs
@@ -1,44 +1,36 @@
-data Tree a = Leaf | Red a (Tree a) (Tree a) | Black a (Tree a) (Tree a)
+data Color = Red | Black deriving Eq
+data Tree a = Leaf | Node Color a (Tree a) (Tree a)
   deriving Eq
 
 
-valueOf (Red v _ _) = v
-valueOf (Black v _ _) = v
+color Leaf = Black
+color (Node c _ _ _) = c
 
-right Leaf = Leaf
-right (Red _ _ r) = r
-right (Black _ _ r) = r
+valueOf (Node _ v _ _) = v
 
 left Leaf = Leaf
-left (Red _ l _) = l
-left (Black _ l _) = l
+left (Node _ _ l _) = l
 
-isRed (Red _ _ _) = True
-isRed _ = False
+right Leaf = Leaf
+right (Node _ _ _ r) = r
 
+isRed   = (== Red) . color
 isBlack = not . isRed
 
+makeRed   (Node c v l r) = Node Red   v l r
+makeBlack (Node c v l r) = Node Black v l r
 
-rotateLeft :: (Tree a) -> (Tree a)
-rotateLeft Leaf = Leaf
-rotateLeft (Red   v a (Red v' b c)) = Red   v' (Red v a b) c
-rotateLeft (Black v a (Red v' b c)) = Black v' (Red v a b) c
+
+rotateLeft (Node c v x (Node Red v' y z)) = Node c v' (Node Red v x y) z
 rotateLeft tree = tree
 
 
-rotateRight Leaf = Leaf
-rotateRight (Red   v (Red v' a b) c) = Red   v' a (Red v b c)
-rotateRight (Black v (Red v' a b) c) = Black v' a (Red v b c)
+rotateRight (Node c v (Node Red v' x y) z) = Node c v' x (Node Red v y z)
 rotateRight tree = tree
 
 
-makeBlack tree = Black (valueOf tree) (left tree) (right tree)
-
-makeRed tree = Red (valueOf tree) (left tree) (right tree)
-
-
 flipColors Leaf = Leaf
-flipColors tree = Red (valueOf tree) (makeBlack $ left tree) (makeBlack $ right tree)
+flipColors (Node _ v l r) = Node Red v (makeBlack l) (makeBlack r)
 
 
 fixTree Leaf = Leaf
@@ -52,22 +44,16 @@ fixTree tree
 
 
 insert value = makeBlack . insert' value
-
-insert' value Leaf = Red value Leaf Leaf
-insert' value tree @ (Red v l r)
-  | value == v = tree
-  | value <  v = fixTree $ Red v (go l) r
-  | value >  v = fixTree $ Red v l (go r)
-  where go = insert' value
-insert' value tree @ (Black v l r)
-  | value == v = tree
-  | value <  v = fixTree $ Black v (go l) r
-  | value >  v = fixTree $ Black v l (go r)
-  where go = insert' value
+  where insert' value Leaf = Node Red value Leaf Leaf
+        insert' value tree @ (Node c v l r)
+          | value == v = tree
+          | value <  v = fixTree $ Node c v (go l) r
+          | value >  v = fixTree $ Node c v l (go r)
+          where go = insert' value
 
 
-insertAll (x:xs) tree = insert x (insertAll xs tree)
-insertAll [] tree = tree
+insertAll (x:xs) = insertAll xs . insert x
+insertAll [] = id
 
 
 {-printInsert :: (Show a) => [a] -> Tree a -> IO ()-}
@@ -80,12 +66,9 @@ printInsert (x:xs) tree = do
 
 instance (Show a) => Show (Tree a) where
   show Leaf = "#"
-  show tree = 
-    "(" ++ color ++ show v ++ " -> " ++ show l ++ " " ++ show r ++ ")"
+  show tree @ (Node _ v l r) =
+    "(" ++ color ++ (show v) ++ " -> " ++ (show l) ++ " " ++ (show r) ++ ")"
     where color = if isRed tree then "R " else "B "
-          v = valueOf tree
-          l = left tree
-          r = right tree
 
 
 main = do
